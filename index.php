6ea6d7c4e0d4d71d5d513dbb593cc7f0e14ca9de
robinhlgrsn <robinholgersson91@gmail.com>
2021-01-13 13:23:53 +0100

    form added to index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+
+<body>
+
+    <form action="./results.php" method="post">
+        <input type="text" name="ape" placeholder="Apor">
+        <input type="text" name="giraff" placeholder="Giraffer">
+        <input type="text" name="tiger" placeholder="Tigrar">
+        <input type="text" name="coco" placeholder="Kokosnötter">
+        <Button type="submit">Save</Button>
+    </form>
+</body>
+
+</html>
